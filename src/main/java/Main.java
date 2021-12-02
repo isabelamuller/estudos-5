0aa7ac33b56be0fa1d53dbab7c75f431d7397179
Isabela Müller <isabelamuller1@hotmail.com>
2021-12-02 17:01:36 -0300

    adicionei a opcao de ver qnts vzs errou

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Escreve um númerozinho: ");
+        System.out.println("Escreve um númerozinho de 0 a 10: ");
         int x = sc.nextInt();
 
+        int count = 0;
         while (x != 9) {
-            System.out.println("Escreve o número 9... ");
-            System.out.println("Escreve um númerozinho: ");
+            System.out.println("Tenta de novo... ");
+            System.out.println("Escreve outro número: ");
             x = sc.nextInt();
+            count++;
         }
+        System.out.println("Tu tentou " + count + " vezes até acertar o número!!!");
     }
 }
